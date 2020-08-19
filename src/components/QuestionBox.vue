8756a03b9445c554f520f4b6fa4d44734893ac6a
DavideDR <d.dr@libero.it>
2020-08-19 10:39:32 +0200

    Adds basic UI

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="jumbotron question-box-container">
+        <p class="lead">
+            Question
+        </p>
+        <hr class="my-4">
+        <p>List of answers</p>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Submit</a>
+        <a class="btn btn-primary btn-lg" href="#" role="button">Next question</a>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// import 'bbotstrap/dist/css/bootstrap.css'
+// import 'bbotstrap/dist/css/bootstrap-vue.css'
+
+//import 'node_modules/bootstrap/dist/css/bootstrap.css'
+//import 'node_modules/botstrap/dist/css/bootstrap-vue.css'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
